9c1b382b4592d99b0aa97fff16054015d3c8043e
jack.squirrel <t.ismagilov@longgammalabs.com>
2025-05-05 11:18:04 +0200

    Increased start block to 13248486

--- apps/web/src/views/V3Info/constants.ts
+++ apps/web/src/views/V3Info/constants.ts
@@ -63,7 +63,7 @@ export const SUBGRAPH_START_BLOCK = {
   [ChainId.LINEA]: 1444,
   [ChainId.BASE]: 2912007,
   [ChainId.OPBNB]: 1721753,
-  [ChainId.ETHERLINK]: 12200266,
+  [ChainId.ETHERLINK]: 13248486,
   [ChainId.ETHERLINK_TESTNET]: 4190090,
 }
 
