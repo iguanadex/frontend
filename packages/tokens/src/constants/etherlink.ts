209d06449787ba0f8b9ac993ecfd106b14aeaf9a
Rohan Tangri <rohan.tangri@gmail.com>
2025-06-18 09:27:45 +0100

    add LBTC logo

--- packages/tokens/src/constants/etherlink.ts
+++ packages/tokens/src/constants/etherlink.ts
@@ -75,4 +75,13 @@ export const etherlinkTokens = {
     'Staked XTZ',
     'https://stacy.fi/',
   ),
+  lbtc: new ERC20Token(
+    ChainId.ETHERLINK,
+    '0xecAc9C5F704e954931349Da37F60E39f515c11c1',
+    8,
+    'LBTC',
+    'LBTC',
+    'Lombard Staked BTC',
+    'https://www.lombard.finance/app/stake/',
+  ),
 }
